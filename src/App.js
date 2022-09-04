d48e2aa9a40403b3dc17856c24601f1af5cc6ab0
uday k reddy <udaykiranreddy8050@gmail.com>
2022-09-04 10:27:28 +0530

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Nav from "./Nav";
 import Details from "./Details";
-import Home from './Home';
 import Item from './Item';
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -9,10 +7,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
-      <Nav />
       <Routes>
-        <Route  path="/" exact element={<Home/>} />
-        <Route path="/Details" exact element={<Details/>} />
+       
+        <Route path="/" exact element={<Details/>} />
         <Route path="/Details/:id" element={<Item/>} />
       </Routes>
     </Router>
